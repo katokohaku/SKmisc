c83038939abdf8e2a92201de8f55ec8be247ec6b
Satoshi Kato <satoshi-2.kato@miraca.com>
2019-01-22 15:05:23 +0900

    modify: split functions.

--- R/split_cv_iterator.R
+++ R/split_cv_iterator.R
@@ -1,18 +1,50 @@
 #' @title
-#' Iterator preparation for cross validation
+#' Generate split iterator pair.
+#' 
 #' @description
-#' Iterator preparation for n-fold & leave-one-out (loo) cross validation\cr
-#' deviding almost equally sizes of train with test subsets.\cr
+#' Generate split iterator pair from iterator size, sample size or fraction rate.\cr
 #' NOTE: Iterator will not be shuffled
-#' @param
-#' size [integer]\cr
-#' Number of data, sample size. Usually NROW(data.frame). 
-#' @param
-#' fold [integer]\cr
-#' Number of folding. If missing, set loo-mode(loo=TRUE).
-#' @param
-#' loo [logical]\cr
-#' Default is FALSE. If TRUE, set loo-mode(loo=TRUE).
+#' 
+#' @param size [An integer] Data size. Usually NROW(data.frame). 
+#' @param n [An integer] sample size. If missing, set n = size * frac.
+#' @param frac  [A numeric] fraction rate. Must be between 0 and 1. Default is 0.5.
+#' 
+#' @examples
+#' splitIterator(100, 40)
+#' 
+#' # The same as above:
+#' splitIterator(100, frac=0.4)
+#' 
+#' @return a list of sampled and remained pair of iterators.
+#' @export
+
+splitIterator <- function(size, n, frac = 0.5)
+{
+  stopifnot(!missing(size))
+  if(missing(n)){ n <- as.integer(size * frac) }
+
+  if(n <= 0 | n > size){
+    stop("sample size is out of data size")
+  }
+  
+  sampled  <- sort(sample(size, n))
+  remained <- (1:size)[-sampled]
+  
+  list(sample = sampled, remain = remained)
+  
+}
+
+
+
+
+#' @title
+#' Iterator splitting for cross validation
+#' @description
+#' Iterator splitting for n-fold & leave-one-out (loo) cross validation deviding almost equally sizes of train with test subsets.\cr
+#' NOTE: Iterator will not be shuffled
+#' @param size [An integer] Number of data, sample size. Usually NROW(data.frame). 
+#' @param fold [An integer] Number of folding. If missing, set loo = TRUE.
+#' @param loo  [A logical] Default is FALSE. If TRUE, set loo-mode (fold = size).
 #' 
 #' @examples
 #' # 4-fold cross
@@ -30,7 +62,7 @@
 #' @return a list
 #' @export
 
-Split_CV_Iterator <- function(size, fold, loo=FALSE)
+splitIteratorCV <- function(size, fold, loo=FALSE)
 {
   stopifnot(!missing(size))
   if(missing(fold)){ loo=TRUE }
--- R/split_frac.R
+++ R/split_frac.R
@@ -1,21 +1,20 @@
 #' @title 
 #' Devide a matrix or a data.frame into two subsets.
+#' 
 #' @description
 #' Similar function as dplyr::sample_frac but return both sampled and remained subset.
 #' 
-#' @param
-#' tbl  [a matrix] or [a data.frame]
-#' @param 
-#' size [a mumeric]\cr 
-#' Sampling ration against NROW(tbl), must be in (0..1]. 
+#' @param  tbl  [a matrix] or [a data.frame] to be split.
+#' @param  size [a mumeric] Sampling ratio. Must be between 0 and 1. 
 #'  
 #' @examples
-#' sep_iris <- Split_Frac(iris,0.8)
+#' sep_iris <- splitFrac(iris, 0.8)
 #' str(sep_iris)
 #' 
+#' @return a list
 #' @export
 
-Split_Frac <- function(tbl, size=1)
+splitFrac <- function(tbl, size=1)
 {
   stopifnot(NROW(tbl)>1, size<=1, size>0)
   pos <- sample(NROW(tbl), size*NROW(tbl), replace=FALSE)
